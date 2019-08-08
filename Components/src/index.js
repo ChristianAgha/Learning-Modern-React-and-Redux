4acc73c3c98c36b3353e031418fa124778c900b3
Christian Agha <christian@Christians-iMac.fios-router.home>
2019-08-08 01:19:43 -0400

    adding more props

--- Components/src/index.js
+++ Components/src/index.js
@@ -1,17 +1,31 @@
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import CommentDetail from './CommentDetail';
+import React from "react";
+import ReactDOM from "react-dom";
+import CommentDetail from "./CommentDetail";
+import faker from "faker";
 
 const App = () => {
-    return (
-        <div className="ui container comments">
-            <CommentDetail author="Sam"/>
-            <CommentDetail author="Alex"/>
-            <CommentDetail author="Jane"/>
-        </div>
-
-    );
+  return (
+    <div className="ui container comments">
+      <CommentDetail
+        author="Sam"
+        timeAgo="Today at 4:33PM"
+        commentContent="Very nice!"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Alex"
+        timeAgo="Today at 9:42AM"
+        commentContent="Noice!"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Jane"
+        timeAgo="Today at 11:03PM"
+        commentContent="I like it!"
+        avatar={faker.image.avatar()}
+      />
+    </div>
+  );
 };
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector("#root"));
