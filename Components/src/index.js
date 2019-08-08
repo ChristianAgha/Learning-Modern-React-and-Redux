1a6f05a69fdb9311ded02f92339783e66f2bb2bf
Christian Agha <christian@Christians-iMac.fios-router.home>
2019-08-08 16:47:42 -0400

    built approval card component

--- Components/src/index.js
+++ Components/src/index.js
@@ -2,30 +2,37 @@ import React from "react";
 import ReactDOM from "react-dom";
 import CommentDetail from "./CommentDetail";
 import faker from "faker";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
-  return (
-    <div className="ui container comments">
-      <CommentDetail
-        author="Sam"
-        timeAgo="Today at 4:33PM"
-        commentContent="Very nice!"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Alex"
-        timeAgo="Today at 9:42AM"
-        commentContent="Noice!"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Jane"
-        timeAgo="Today at 11:03PM"
-        commentContent="I like it!"
-        avatar={faker.image.avatar()}
-      />
-    </div>
-  );
+    return (
+        <div className="ui container comments">
+            <ApprovalCard>
+                <CommentDetail
+                    author="Sam"
+                    timeAgo="Today at 4:33PM"
+                    commentContent="Very nice!"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author="Alex"
+                    timeAgo="Today at 9:42AM"
+                    commentContent="Noice!"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author="Jane"
+                    timeAgo="Today at 11:03PM"
+                    commentContent="I like it!"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+        </div>
+    );
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
