ae482ff07f493b3f9e36d3a855a379eda706938d
Christian Agha <christian@Christians-iMac.fios-router.home>
2019-08-25 01:13:21 -0400

    added axios and connected to unsplash API

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
 
     onSearchSubmit(term) {
-        console.log(term);
+        axios.get('https://api.unsplash.com/search/photos', {
+            params: { query: term },
+            headers: {
+                Authorization: 'Client-ID 4663c1bef721e540d62743c27abc02d122c920c447e5bb293ca3ecaba2200c7a'
+
+            }
+        });
     }
 
     render() {
