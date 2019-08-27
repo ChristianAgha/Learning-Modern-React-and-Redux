476a2c5b607855cc1d4a96e944fc58076e3de3b2
Christian Agha <christian@Christians-iMac.fios-router.home>
2019-08-27 15:46:31 -0400

    added video detail component:

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -2,25 +2,29 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 class App extends React.Component {
 
-    state = { videos: [] };
+    state = { videos: [], selectedVideo: null };
 
     onSearchTermSubmit = async SearchTerm => {
         const response = await youtube.get('/search', {
-            params: {
-                q: SearchTerm
-            }
+            params: { q: SearchTerm }
         });
 
         this.setState({ videos: response.data.items });
     };
 
+    onVideoSelect = (video) => {
+        this.setState({selectedVideo: video});
+    };
+
     render() {
         return (<div className="ui container">
                     <SearchBar onSearchTermSubmit={this.onSearchTermSubmit}/>
-                    <VideoList videos={this.state.videos} />
+                    <VideoDetail video={this.state.selectedVideo} />
+                    <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
                 </div>);
     };
 }
--- videos/src/components/VideoList.js
+++ videos/src/components/VideoList.js
@@ -2,10 +2,10 @@ import React from 'react';
 import VideoItem from './VideoItem';
 
 // { videos } below is destructured props.videos
-const VideoList = ({ videos }) => {
+const VideoList = ({ videos, onVideoSelect }) => {
 
     const renderedList = videos.map((video) => {
-        return <VideoItem video={video}/>;
+        return <VideoItem onVideoSelect={onVideoSelect} video={video}/>;
     });
 
     // props.videos is the array of videos that we recive that we want to render to the screen
