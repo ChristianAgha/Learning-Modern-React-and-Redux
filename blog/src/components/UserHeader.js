617e742d44a7400ef42f14a3f51c9e6ea330c116
Christian Agha <christian@Christians-iMac.fios-router.home>
2019-09-26 17:18:53 -0400

    added UserHeader class based component

---
+++ blog/src/components/UserHeader.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class UserHeader extends React.Component {
+    render() {
+        return (
+            <div>
+                User header
+            </div>
+        )
+    }
+}
+
+export default UserHeader;
